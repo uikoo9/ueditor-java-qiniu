ce387a156f620bed94db1bf4e674fbee67bd5bec
uikoo9 <uikoo9@qq.com>
2015-07-29 08:36:54 +0800

    新版本ok

--- WebRoot/ueditor-min-1.4.3/ueditor.config.js
+++ WebRoot/ueditor-min-1.4.3/ueditor.config.js
@@ -88,7 +88,7 @@
         //,iframeCssUrl: URL + '/themes/iframe.css' //给编辑器内部引入一个css文件
 
         //,initialFrameWidth:1000  //初始化编辑器宽度,默认1000
-    	,initialFrameHeight:320  //初始化编辑器高度,默认320
+    	,initialFrameHeight:640  //初始化编辑器高度,默认320
 
         //,readonly : false //编辑器初始化结束后,编辑区域是否是只读的，默认是false
 
